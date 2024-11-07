60f04eb52f37001ca3d31f3d8cec6a38a43e9f93
PEGADURO\jorgeav <analista@pegaduro.com.mx>
2024-11-07 08:50:43 -0600

    hcamacho: creacion y asignacion de valor a una variable. Solicitar informacion al usuario con la instruccion input

---
+++ Variables_Asignaciones/Variables_Y_Asignaciones.py
@@ -0,0 +1,13 @@
+#Asignación de valor a una variable
+w = 39
+print(type(w))
+
+z = 10.8 + 12.2 + 0.2
+print(z**4)
+
+#Pedirle información al usuario
+nombre = input("Ingresa tu nombre: ")
+print(nombre)
+
+f = float(input("Ingresa un numero flotante: "))
+print(f**2)
